12e0f40e33df98cb5e0365a6c2677162450a4001
Samangelof <ksa1sano@mail.ru>
2024-05-14 06:41:27 +0300

    bot initialization

---
+++ botbread/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
+
+Base = declarative_base()
+
+class UserRequest(Base):
+    __tablename__ = 'user_requests'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    user_id = Column(Integer)
+    request_time = Column(DateTime, default=datetime.utcnow)
+    request_text = Column(Text)
+
+
+engine = create_engine('sqlite:///user_requests.db')
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
